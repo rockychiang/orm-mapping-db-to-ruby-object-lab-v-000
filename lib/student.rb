44de9392346cfe1b5fb65e1fdb8855f1400bc90d
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 04:44:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.first_x_students_in_grade_10
+  def self.first_x_students_in_grade_10(X)
     sql = <<-SQL
     SELECT COUNT(grade) 
     FROM students
