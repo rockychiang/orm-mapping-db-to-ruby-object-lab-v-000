543c2747153a766be9875b7ba6033c11edf26ce6
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 04:28:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,9 @@ class Student
 
   def self.new_from_db(row)
     Student.new.tap do |student|
-      
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
     end
   end
 
