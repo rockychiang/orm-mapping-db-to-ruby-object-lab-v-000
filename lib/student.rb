ca614ccc44d9782e95313ebedf982585e117ac11
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 04:46:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,16 @@ class Student
     DB[:conn].execute(sql, x)
   end
   
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
