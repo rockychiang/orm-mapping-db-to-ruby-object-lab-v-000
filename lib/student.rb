9fbb3cd063f13170759953a1b51b151e238d2cc7
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 04:46:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     DB[:conn].execute(sql, x)
   end
   
-  def self.first_X_students_in_grade_10(x)
+  def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
